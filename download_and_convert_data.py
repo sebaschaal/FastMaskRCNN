4cda4c6db68158ee5449aa5e4c0d5da64fd58c07
Sebastian Schaal <schaal@magazino.eu>
2017-04-11 10:03:46 +0200

    load just one file

--- download_and_convert_data.py
+++ download_and_convert_data.py
@@ -11,25 +11,25 @@ from libs.datasets import download_and_convert_coco
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string(
-    'dataset_name', 'coco',
+    'dataset_name2', 'coco',
     'The name of the dataset to convert, one of "cifar10", "flowers", "mnist".')
 
 tf.app.flags.DEFINE_string(
-    'dataset_dir', 'data/coco',
+    'dataset_dir2', 'data/coco',
     'The directory where the output TFRecords and temporary files are saved.')
 
 
 def main(_):
-  if not FLAGS.dataset_name:
+  if not FLAGS.dataset_name2:
     raise ValueError('You must supply the dataset name with --dataset_name')
-  if not FLAGS.dataset_dir:
+  if not FLAGS.dataset_dir2:
     raise ValueError('You must supply the dataset directory with --dataset_dir')
 
   elif FLAGS.dataset_name == 'coco':
-    download_and_convert_coco.run(FLAGS.dataset_dir)
+    download_and_convert_coco.run(FLAGS.dataset_dir2)
   else:
     raise ValueError(
-        'dataset_name [%s] was not recognized.' % FLAGS.dataset_dir)
+        'dataset_name [%s] was not recognized.' % FLAGS.dataset_dir2)
 
 if __name__ == '__main__':
   tf.app.run()
